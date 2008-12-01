e80f531478a9d2d8296315b14d5f7539a8cc7d6f
jpmassena <jpmassena@2f997a58-4d4e-0410-88ba-2daf01c66da7>
2008-12-01 18:35:39 +0000

    Version 0.5

---
+++ trunk/tvdb-api/Test/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿/*
+ * This file is part of TVDBDLL.
+ *
+ *  TVDBDLL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TVDBDLL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TVDBDLL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lvSearchResult = new System.Windows.Forms.ListView();
+            this.chId = new System.Windows.Forms.ColumnHeader();
+            this.chName = new System.Windows.Forms.ColumnHeader();
+            this.chOverview = new System.Windows.Forms.ColumnHeader();
+            this.chLang = new System.Windows.Forms.ColumnHeader();
+            this.chBanner = new System.Windows.Forms.ColumnHeader();
+            this.chIMDB = new System.Windows.Forms.ColumnHeader();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "English",
+            "Dansk",
+            "Suomeksi",
+            "Dutch",
+            "Deutsch",
+            "Italiano",
+            "Español",
+            "Français",
+            "Polski",
+            "Magyar",
+            "Ελληνικά",
+            "Türkçe",
+            "русский язык",
+            "עברית",
+            "日本語",
+            "Português",
+            "中文",
+            "čeština",
+            "Slovenski",
+            "English",
+            "Svenska",
+            "Norsk"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // lvSearchResult
+            // 
+            this.lvSearchResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chId,
+            this.chName,
+            this.chOverview,
+            this.chLang,
+            this.chBanner,
+            this.chIMDB});
+            this.lvSearchResult.FullRowSelect = true;
+            this.lvSearchResult.Location = new System.Drawing.Point(162, 27);
+            this.lvSearchResult.MultiSelect = false;
+            this.lvSearchResult.Name = "lvSearchResult";
+            this.lvSearchResult.Size = new System.Drawing.Size(908, 469);
+            this.lvSearchResult.TabIndex = 1;
+            this.lvSearchResult.UseCompatibleStateImageBehavior = false;
+            this.lvSearchResult.View = System.Windows.Forms.View.Details;
+            this.lvSearchResult.DoubleClick += new System.EventHandler(this.lvSearchResult_DoubleClick);
+            // 
+            // chId
+            // 
+            this.chId.Text = "Id";
+            this.chId.Width = 61;
+            // 
+            // chName
+            // 
+            this.chName.Text = "Name";
+            this.chName.Width = 198;
+            // 
+            // chOverview
+            // 
+            this.chOverview.Text = "Overview";
+            this.chOverview.Width = 351;
+            // 
+            // chLang
+            // 
+            this.chLang.Text = "Language";
+            this.chLang.Width = 68;
+            // 
+            // chBanner
+            // 
+            this.chBanner.Text = "Banner";
+            this.chBanner.Width = 158;
+            // 
+            // chIMDB
+            // 
+            this.chIMDB.Text = "IMDB_ID";
+            this.chIMDB.Width = 66;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(2, 502);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1068, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 150);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "actors";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(12, 173);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "banners";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(12, 127);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(48, 17);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "serie";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 533);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lvSearchResult);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView lvSearchResult;
+        private System.Windows.Forms.ColumnHeader chId;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader chLang;
+        private System.Windows.Forms.ColumnHeader chBanner;
+        private System.Windows.Forms.ColumnHeader chOverview;
+        private System.Windows.Forms.ColumnHeader chIMDB;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ trunk/tvdb-api/Test/Form1.cs
@@ -0,0 +1,297 @@
+﻿/*
+ * This file is part of TVDBDLL.
+ *
+ *  TVDBDLL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TVDBDLL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TVDBDLL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TVDBDLL;
+
+namespace Test
+{
+    /// <summary>
+    /// Main form
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Delegate to call the dataReceiverHandler.
+        /// </summary>
+        /// <param name="data">The Data.</param>
+        /// <param name="type">The Type of the data.</param>
+        public delegate void dataCall(object data, int type);
+
+        /// <summary>
+        /// Delegate to call the errorHandler.
+        /// </summary>
+        /// <param name="exc">The Exception.</param>
+        public delegate void errorCall(Exception exc);
+
+        /// <summary>
+        /// Delegate to call the progressUpdater.
+        /// </summary>
+        /// <param name="value">Value to add to the progress bar.</param>
+        public delegate void progressCall(int value);
+
+        /// <summary>
+        /// Event of errorCall.
+        /// </summary>
+        private event errorCall formErrorCaller;
+
+        /// <summary>
+        /// Event of dataReceivedCall.
+        /// </summary>
+        private event dataCall formDataCaller;
+
+        /// <summary>
+        /// Event of progressCall.
+        /// </summary>
+        private event progressCall formProgressCaller;
+
+        /// <summary>
+        /// Object that holds reference to tvdb connection.
+        /// </summary>
+        private TVDB tvDbHandler;
+
+        /// <summary>
+        /// Constructor of the form.
+        /// </summary>
+        public Form1()
+        {
+            formErrorCaller += new errorCall(errorHandler);
+            formProgressCaller += new progressCall(progressUpdater);
+            formDataCaller += new dataCall(dataReceiver);
+
+            InitializeComponent();
+            comboBox1.Text = comboBox1.Items[0].ToString();
+        }
+
+        /// <summary>
+        /// Change the progress bar value.
+        /// </summary>
+        /// <param name="value">Value to add to the current value.</param>
+        public void progressUpdater(int value)
+        {
+            progressBar1.Value = value;
+        }
+
+        /// <summary>
+        /// Receives data from the tvdbDLL.
+        /// </summary>
+        /// <param name="data">The Data.</param>
+        /// <param name="type">The Type of the data.</param>
+        public void dataReceiver(object data, int type)
+        {
+            if (type == 0)//all
+            {
+                List<String> results = (List<String>) data;
+                Console.WriteLine(results[0]);//serie info
+                Console.WriteLine(results[1]);//actors info
+                Console.WriteLine(results[2]);//banners info
+            }
+            else if (type == 1)// serie and actors 
+            {
+                List<String> results = (List<String>)data;
+                Console.WriteLine(results[0]);//serie info
+                Console.WriteLine(results[1]);//actors info
+            }
+            else if (type == 2)// serie and banners 
+            {
+                List<String> results = (List<String>)data;
+                Console.WriteLine(results[0]);//serie info
+                Console.WriteLine(results[2]);//banners info
+            }
+            else if (type == 3)// serie 
+            {
+                List<String> results = (List<String>)data;
+                Console.WriteLine(results[0]);//serie info
+            }
+            else if (type == 4)// actors
+            {
+                List<String> results = (List<String>)data;
+                Console.WriteLine(results[1]);//actors info
+            }
+            else if (type == 5)//banners 
+            {
+                List<String> results = (List<String>)data;
+                Console.WriteLine(results[2]);//banners info
+            }
+            else if (type == 5)//actors and banners 
+            {
+                List<String> results = (List<String>)data;
+                Console.WriteLine(results[1]);//actors info
+                Console.WriteLine(results[2]);//banners info
+            }
+        }
+
+        /// <summary>
+        /// Displays a message box with the error that occured.
+        /// </summary>
+        /// <param name="exc">Exception occured.</param>
+        public void errorHandler(Exception exc)
+        {
+            button1.Enabled = true;
+            Cursor = Cursors.Default;
+            progressBar1.Value = 0;
+            MessageBox.Show(exc.StackTrace, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Function that handles the search button click. It is here that the API is called.
+        /// </summary>
+        /// <param name="sender">default argument.</param>
+        /// <param name="e">default argument.</param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            progressBar1.Value = 0;
+            lvSearchResult.Items.Clear();
+            tvDbHandler = new TVDB();
+
+            tvDbHandler.parentErrorCaller = formErrorCaller;
+            tvDbHandler.parentProgressUpdaterCaller = formProgressCaller;
+            tvDbHandler.parentDataReceiverCaller = formDataCaller;
+            String lang = getLanguage();
+            List<ResultItem> results = tvDbHandler.searchSerie(lang, textBox1.Text);
+
+            foreach (ResultItem r in results)
+            {
+                ListViewItem item = new ListViewItem(r.ID);
+                item.SubItems.Add(r.SerieName);
+                item.SubItems.Add(r.Overview);
+                item.SubItems.Add(r.Language);
+                item.SubItems.Add(r.Banner);
+                item.SubItems.Add(r.IMDB_ID);
+                item.Tag = r;
+                lvSearchResult.Items.Add(item);
+            }
+            button1.Enabled = true;
+        }
+
+        /// <summary>
+        /// Changes the selected language to the site's format
+        /// </summary>
+        /// <returns>The language in the site's format</returns>
+        private String getLanguage()
+        {
+            String l = comboBox1.Text;
+            if (l == "English")
+            {
+                return "en";
+            }
+            else if (l == "Dansk")
+            {
+                return "da";
+            }
+            else if (l == "Suomeksi")
+            {
+                return "fi";
+            }
+            else if (l == "Dutch")
+            {
+                return "nl";
+            }
+            else if (l == "Deutsch")
+            {
+                return "de";
+            }
+            else if (l == "Italiano")
+            {
+                return "it";
+            }
+            else if (l == "Español")
+            {
+                return "es";
+            }
+            else if (l == "Français")
+            {
+                return "fr";
+            }
+            else if (l == "Polski")
+            {
+                return "pl";
+            }
+            else if (l == "Magyar")
+            {
+                return "hu";
+            }
+            else if (l == "Ελληνικά")
+            {
+                return "el";
+            }
+            else if (l == "Türkçe")
+            {
+                return "tr";
+            }
+            else if (l == "русский язык")
+            {
+                return "ru";
+            }
+            else if (l == "עברית")
+            {
+                return "he";
+            }
+            else if (l == "日本語")
+            {
+                return "ja";
+            }
+            else if (l == "Português")
+            {
+                return "pt";
+            }
+            else if (l == "中文")
+            {
+                return "zh";
+            }
+            else if (l == "čeština")
+            {
+                return "cs";
+            }
+            else if (l == "Slovenski")
+            {
+                return "sl";
+            }
+            else if (l == "Svenska")
+            {
+                return "sv";
+            }
+            else if (l == "Norsk")
+            {
+                return "no";
+            }
+            return "en";
+        }
+
+        /// <summary>
+        /// Here we fetch the info of selected serie
+        /// </summary>
+        /// <param name="sender">default argument.</param>
+        /// <param name="e">default argument.</param>
+        private void lvSearchResult_DoubleClick(object sender, EventArgs e)
+        {
+            if (lvSearchResult.SelectedItems.Count == 1)
+            {
+                ResultItem selected = (ResultItem)lvSearchResult.SelectedItems[0].Tag;
+                tvDbHandler.DownloadSerieZipped(selected.ID, checkBox3.Checked, checkBox1.Checked, checkBox2.Checked);
+            }
+        }
+    }
+}
